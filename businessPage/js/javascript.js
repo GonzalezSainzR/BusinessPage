a76662ce197b135ff224c4d7e36c7a95d9b1558a
Renan Gonzalez-Sainz <raygs2000@gmail.com>
2015-09-29 13:59:26 -0700

    Fixed stuff

--- businessPage/js/javascript.js
+++ businessPage/js/javascript.js
@@ -1,4 +1,4 @@
-window.smoothScroll = function(target) {
+window.smoothScroll = function (target) {
     var scrollContainer = target;
     do { //find scroll container
         scrollContainer = scrollContainer.parentNode;
@@ -12,11 +12,15 @@ window.smoothScroll = function(target) {
         targetY += target.offsetTop;
     } while (target = target.offsetParent);
 
-    scroll = function(c, a, b, i) {
-        i++; if (i > 30) return;
-        c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){ scroll(c, a, b, i); }, 20);
-    }
-    // start scrolling
+    scroll = function (c, a, b, i) {
+            i++;
+            if (i > 30) return;
+            c.scrollTop = a + (b - a) / 30 * i;
+            setTimeout(function () {
+                scroll(c, a, b, i);
+            }, 20);
+        }
+        // start scrolling
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
+
